c2c070d349125310057b27ae16dfe9186b569f46
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-08 00:46:07 +0100

    insert node to right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,17 +8,20 @@
  **/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *insert_right = malloc(sizeof(binary_tree_t));
+	binary_tree_t *insert_right;
 
-	if (insert_right == NULL || parent == NULL)
+	if (parent == NULL)
 		return (NULL);
+	insert_right = binary_tree_node(parent, value);
+	if (insert_right == NULL)
+		return (NULL);
+	insert_right->parent = parent;
+	parent->right = insert_right;
 	if (parent->right != NULL)
 	{
 		parent->right->parent = insert_right;
 		insert_right->right = parent->right;
 	}
-	parent->right = insert_right;
-	insert_right->parent = parent;
 	insert_right->n = value;
 
 	return (insert_right);
