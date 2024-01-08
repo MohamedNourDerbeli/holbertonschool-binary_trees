bc5ffcd5205d286af775c919e3a2289d499abe5d
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-08 02:21:59 +0100

    add inorder

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 /**
- * binary_tree_inorder - Traverse a binary tree using in-order traversal.
+ * binary_tree_postorder - Traverse a binary tree using post-order traversal.
  * @tree: Pointer to the root node of the tree to traverse.
  * @func: Pointer to a function to call for each node's value.
  *
  * Description: This function recursively traverses a binary tree using
- * in-order traversal, processing the left subtree, the current node, and then
- * the right subtree. The provided function is applied to each node's value.
- * If the tree or the function is NULL, the function does nothing.
+ * post-order traversal, processing the left subtree, the right subtree, and
+ * then the current node. The provided function is applied to each node's
+ * value. If the tree or the function is NULL, the function does nothing.
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
