cf471691c04ba2f0a5f848af89f0e9488e012c84
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-09 16:03:06 +0100

    binary_tree_uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,15 +7,15 @@
  **/
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (!node || !node->parent)
-        return (NULL);
+	if (!node || !node->parent)
+		return (NULL);
 
-    if (node->parent->left == node)
-        return (node->parent->right);
-    else if (node->parent->right == node)
-        return (node->parent->left);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else if (node->parent->right == node)
+		return (node->parent->left);
 
-    return (NULL);
+	return (NULL);
 }
 
 /**
@@ -25,8 +25,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
  **/
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (!node || !node->parent)
-        return (NULL);
+	if (!node || !node->parent)
+		return (NULL);
 
-    return binary_tree_sibling(node->parent);
+	return (binary_tree_sibling(node->parent));
 }
