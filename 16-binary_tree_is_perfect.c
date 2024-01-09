17e9d00d69badec3232a5afa93c4a8c0277b5eba
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-09 14:52:23 +0100

    binary_tree_perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -37,11 +37,14 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (1);
 	if ((tree->left && !tree->right) || (!tree->left && tree->right))
 		return (0);
+	if (tree->left && tree->right)
+	{
+		binary_tree_is_perfect(tree->left);
+		binary_tree_is_perfect(tree->right);
+	}
 
 	le = binary_tree_height(tree->left);
 	ri = binary_tree_height(tree->right);
-	binary_tree_is_perfect(tree->right);
-	binary_tree_is_perfect(tree->left);
 	if (ri == le)
 		return (1);
 
