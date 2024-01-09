dff504f0b7415298b6b7dab5c4f9eb3e74157920
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-01-09 05:00:34 +0100

    binary_tree_nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - Counts the nodes with at least one child in a binary tree.
+ * @tree: Pointer to the root node of the tree.
+ * Return: Number of internal nodes in the binary tree, or 0 if the tree is NULL.
+ **/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	int dist = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left || tree->right)
+		dist += 1;
+
+	dist += binary_tree_nodes(tree->left);
+	dist += binary_tree_nodes(tree->right);
+
+	return (dist);
+}
